81e853f345e89de2757927c63a01070042298ad4
Wellington Pereira <lleco12@gmail.com>
2024-12-02 18:55:52 -0300

    Home

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -4,14 +4,15 @@ import './Home.css';
 function Home() {
     return (
         <section id="inicio">
-        <div className="hero-container">
-            <h1>Peça sua arte</h1>
-            <h2>Arts & Craft</h2>
-            <h3>
-            Decorar nunca foi tão divertido! Nossos itens de cerâmica são feitos à mão
-            com muito amor... e uma dose de caos criativo, para transformar seu espaço!
-            </h3>
-        </div>
+            <div className="hero-container">
+                <h1>Peça sua arte</h1>
+                <h2>Arts & Craft</h2>
+                <h3>
+                Decorar nunca foi tão divertido! Nossos itens de cerâmica são feitos à mão
+                com muito amor... e uma dose de caos criativo, para transformar seu espaço!
+                </h3>
+            </div>
+            <div class="hero-image-container"></div>
         </section>
     )
 }
