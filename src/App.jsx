79c8d580a4f0f9cd7b3218d69b9dc1f375ec9750
Wellington Periera <lleco12@gmail.com>
2025-04-15 10:55:54 -0300

    Update App.jsx
    Salvar carrinho

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Home from './Components/Home/Home';
 import Produtos from './Components/Produtos/Produtos';
@@ -8,24 +8,36 @@ import End from './Components/End/End';
 import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 function App() {
-  const [carrinhoItens, setCarrinhoItens] = useState([]); // Estado centralizado do carrinho
+  // Inicializa o estado com os dados do localStorage, se existirem
+  const [carrinhoItens, setCarrinhoItens] = useState(() => {
+    const savedCart = localStorage.getItem('carrinhoItens');
+    return savedCart ? JSON.parse(savedCart) : [];
+  });
+
+  // Sempre que o carrinho for atualizado, atualiza o localStorage
+  useEffect(() => {
+    localStorage.setItem('carrinhoItens', JSON.stringify(carrinhoItens));
+  }, [carrinhoItens]);
 
   return (
     <HelmetProvider>
       <div>
         <Helmet>
-            <title>Arts & Craft - Artesanatos</title>
-            <link rel="canonical" href="https://Arts&craft.com.br" />
-            <meta name="description" content="Nossos itens de cerâmica são feitos à mão com muito amor... e uma dose de caos criativo, para transformar seu espaço!" />
+          <title>Arts & Craft - Artesanatos</title>
+          <link rel="canonical" href="https://Arts&craft.com.br" />
+          <meta
+            name="description"
+            content="Nossos itens de cerâmica são feitos à mão com muito amor... e uma dose de caos criativo, para transformar seu espaço!"
+          />
         </Helmet>
         <Navbar 
-            carrinhoItens={carrinhoItens}
-            setCarrinhoItens={setCarrinhoItens}
+          carrinhoItens={carrinhoItens}
+          setCarrinhoItens={setCarrinhoItens}
         />
         <Home />
         <Produtos 
-            carrinhoItens={carrinhoItens} 
-            setCarrinhoItens={setCarrinhoItens}
+          carrinhoItens={carrinhoItens} 
+          setCarrinhoItens={setCarrinhoItens}
         /> 
         <Velas /> 
         <SobreNos /> 
